268dfc8474c61417a529062f717269cd201760da
tomas-fryza <tomas.fryza@gmail.com>
2024-04-04 14:36:30 +0200

    concatenate csv files

--- PYTHON/explore_csv.py
+++ PYTHON/explore_csv.py
@@ -1,13 +1,34 @@
-import pandas as pd
+try:
+    import pandas as pd
+    import glob  # Filename pattern matching
+except ImportError as e:
+    print(f"[!] ImportError: {e}")
+    raise SystemExit(1)
 
-filename = "../DATA/sc6-61/_CSV/position_1/sc6-61_2024-02-07_ver-99.csv"
-df = pd.read_csv(filename, sep=";", decimal=".")
+path = "../DATA/sc6-61/_CSV/position_1/"
+dates = "2024-03-"
 
+# Load selected *.csv files into a DataFrame
+csv_files = glob.glob(path + "*" + dates + "*.csv")
+csv_files = sorted(csv_files)
+df = pd.concat(
+    (pd.read_csv(filename, sep=";", decimal=".") for filename in csv_files),
+    ignore_index=True
+)
+
+# Convert timestamp to pandas datetime object
+df["datetime"] = pd.to_datetime(df["datetime"])
+
+print(f"Number of files: {len(csv_files)}")
 print(f"Number of probe requests: {len(df):,}")
 
 # Print basic information about a DataFrame
+print("\nInfo:")
 print(df.info())
 
 # Print all columns of the first 5 lines of dataframe
 pd.set_option('display.max_columns', None)
+print("\nHead:")
 print(df.head())
+print("\nTail:")
+print(df.tail())
